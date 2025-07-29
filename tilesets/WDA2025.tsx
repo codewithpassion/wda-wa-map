504c65b5051c35e8a12d4ead95ac5c0fc40ec874
Dominik Fretz <dominik.fretz@gmail.com>
2025-07-28 19:51:25 -0700

    Initial

---
+++ tilesets/WDA2025.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="WDA2025" tilewidth="32" tileheight="32" tilecount="39" columns="13">
+ <image source="../../../OneDrive/Bilder/Screenshots/Screenshot 2025-07-27 190725.png" width="434" height="119"/>
+</tileset>
---
+++ tilesets/WDASmall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="WDASmall" tilewidth="32" tileheight="32" tilecount="16" columns="8">
+ <image source="../../../OneDrive/Bilder/Screenshots/Smaller.png" width="260" height="71"/>
+</tileset>
